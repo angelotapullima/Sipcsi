c2ca9a9bf0a5adf2c7183fac17d231342029b5d1
anked10 <angelo.anked@gmail.com>
2019-07-09 20:11:14 -0500

    cambio de contraseña efectiva x2

--- app/src/main/java/com/bufeotec/sipcsi/Activitys/CambiarContrasena.java
+++ app/src/main/java/com/bufeotec/sipcsi/Activitys/CambiarContrasena.java
@@ -1,9 +1,8 @@
 package com.bufeotec.sipcsi.Activitys;
 
-<<<<<<< HEAD
+
 import android.content.Intent;
-=======
->>>>>>> origin/master
+
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -12,10 +11,9 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-<<<<<<< HEAD
+
 import android.widget.Toast;
-=======
->>>>>>> origin/master
+
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.DefaultRetryPolicy;
@@ -23,10 +21,9 @@ import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
-<<<<<<< HEAD
+
 import com.bufeotec.sipcsi.Principal.MainActivity;
-=======
->>>>>>> origin/master
+
 import com.bufeotec.sipcsi.R;
 import com.bufeotec.sipcsi.WebServices.VolleySingleton;
 
@@ -37,18 +34,16 @@ import static com.bufeotec.sipcsi.WebServices.DataConnection.IP;
 
 public class CambiarContrasena extends AppCompatActivity {
 
-<<<<<<< HEAD
+
     EditText contraNueva,contraAntigua,contraNuevaRepetida;
-=======
-    EditText contraNueva;
->>>>>>> origin/master
+
     Button btnContra;
     String id ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cambiar_contrasena);
-<<<<<<< HEAD
+
 
 
         contraNueva=findViewById(R.id.contraNueva);
@@ -56,19 +51,15 @@ public class CambiarContrasena extends AppCompatActivity {
         contraNuevaRepetida=findViewById(R.id.contraNuevaRepetida);
         btnContra=findViewById(R.id.btnCambiarContra);
 
-
-=======
-        contraNueva=findViewById(R.id.contraNueva);
-        btnContra=findViewById(R.id.btnCambiarContra);
->>>>>>> origin/master
-        showToolbar("Editar Contraseña",true);
+        showToolbar("Cambiar Contraseña",true);
         id=getIntent().getExtras().getString("id");
 
 
         btnContra.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-<<<<<<< HEAD
+
+
 
                 if (contraNueva.getText().toString().equals(contraNuevaRepetida.getText().toString())){
                     cambiar();
@@ -76,9 +67,8 @@ public class CambiarContrasena extends AppCompatActivity {
                     Toast.makeText(CambiarContrasena.this, "Las Contraseñas no Coinciden", Toast.LENGTH_SHORT).show();
                 }
 
-=======
                 cambiar();
->>>>>>> origin/master
+
             }
         });
     }
@@ -89,7 +79,8 @@ public class CambiarContrasena extends AppCompatActivity {
         stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
-<<<<<<< HEAD
+
+
                 Log.e("respuesta de contraseña", "onResponse: " + response.trim().toString() );
                 if(response.trim().equalsIgnoreCase("1")){
 
@@ -109,16 +100,9 @@ public class CambiarContrasena extends AppCompatActivity {
                     contraAntigua.setText("");
                     contraNueva.setText("");
                     contraNuevaRepetida.setText("");
-=======
-                if(response.trim().equalsIgnoreCase("1")){
-
-                } else {
-                    //Toast.makeText(context,"la fruta ",Toast.LENGTH_SHORT).show();
-                    Log.e("noti_inicio_ruta:",""+response);
->>>>>>> origin/master
-                }
 
 
+            }
             }
 
         }, new Response.ErrorListener() {
@@ -128,7 +112,7 @@ public class CambiarContrasena extends AppCompatActivity {
                 Log.i("RESPUESTA: ",""+error.toString());
 
             }
-        })  {
+        } ) {
             @Override
             protected Map<String, String> getParams() throws AuthFailureError {
                 //String imagen=convertirImgString(bitmap);
@@ -138,12 +122,8 @@ public class CambiarContrasena extends AppCompatActivity {
 
                 Map<String,String> parametros=new HashMap<>();
                 parametros.put("id_usuario",id);
-<<<<<<< HEAD
                 parametros.put("contrasenha_nueva",contraNueva.getText().toString());
                 parametros.put("contrasenha_antigua",contraAntigua.getText().toString());
-=======
-                parametros.put("contrasenha",contraNueva.getText().toString());
->>>>>>> origin/master
 
                 return parametros;
 
